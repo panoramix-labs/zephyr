00dbac95ec5e4a4ef9dfe3549db950312c03e73a
Josuah Demangeon <me@josuah.net>
2024-12-08 02:51:48 +0000

    gdb setup

---
+++ boards/seeed/grove_vision_ai/board.cmake
@@ -0,0 +1,9 @@
+# Copyright (c) 2024 Panoramix Labs
+# SPDX-License-Identifier: Apache-2.0
+
+board_set_debugger_ifnset(pyocd)
+board_set_flasher_ifnset(pyocd)
+
+board_runner_args(pyocd "--target=we2")
+
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
